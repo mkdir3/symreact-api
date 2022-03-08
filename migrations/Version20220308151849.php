7d71003f2b34baa4f8be00e4839b6d543773ef7f
Ted <p0599847@protonmail.com>
2022-03-08 17:08:31 +0100

    update faker with new relations and implementing User entity

---
+++ migrations/Version20220308151849.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220308151849 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE invoice ADD chrono INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE customer CHANGE first_name first_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE last_name last_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE email email VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE company company VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE invoice DROP chrono, CHANGE status status VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,36 +4,76 @@ namespace App\DataFixtures;
 
 use App\Entity\Customer;
 use App\Entity\Invoice;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    /* 
+    * Password hasher
+    * @var UserPasswordHasherInterface
+    */
+
+    private $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // Set faker to fake french datas
         $faker = Factory::create('fr_FR');
-        // Create 30 Customers
-        for($c = 0; $c< 30; $c++) {
-            $customer = new Customer();
-            $customer->setFirstName($faker->firstName)
-                     ->setLastName($faker->lastName)
-                     ->setCompany($faker->company)
-                     ->setEmail($faker->email);
 
-            $manager->persist($customer);
-        // For each customer, make invoices between 3 to 10
-            for($i= 0; $i < mt_rand(3, 10); $i++) {
-                $invoice = new Invoice();
-                $invoice->setAmount($faker->randomFloat(2, 250, 14500))
+        // Creating users
+        for ($u = 0; $u < 10; $u++) {
+            $user = new User();
+
+            // Chrono will be the invoice number
+            $chrono = 1;
+
+            // Hash a random password name
+            $hash = $this->encoder->hashPassword($user, "password");
+
+            $user->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setPassword($hash);
+
+            $manager->persist($user);
+
+            // Create between 5 to 20 Customers
+            for ($c = 0; $c < mt_rand(5, 20); $c++) {
+                $customer = new Customer();
+                $customer->setFirstName($faker->firstName)
+                    ->setLastName($faker->lastName)
+                    ->setCompany($faker->company)
+                    ->setEmail($faker->email)
+                    ->setUser($user);
+
+
+                $manager->persist($customer);
+                // For each customer, make invoices between 3 to 10
+                for ($i = 0; $i < mt_rand(3, 10); $i++) {
+                    $invoice = new Invoice();
+                    $invoice->setAmount($faker->randomFloat(2, 250, 14500))
                         ->setSentAt($faker->dateTimeBetween('-6months'))
                         ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
-                        ->setCustomer($customer);
+                        ->setCustomer($customer)
+                        ->setChrono($chrono);
 
-                $manager->persist($invoice);
+                    $chrono++;
+
+                    $manager->persist($invoice);
+                }
             }
         }
+
+
 
         $manager->flush();
     }
